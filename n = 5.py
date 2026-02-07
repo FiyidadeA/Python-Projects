caebd1e8acca32739f42b95b50e5062e7511e070
FiyidadeA <fiyidads@gmail.com>
2026-02-07 16:15:10 +0000

    CLI tracker project

--- n = 5.py
+++ n = 5.py
@@ -3,6 +3,4 @@ for nums in (25, 54 , 55, 12, 38, 9, 99, 22, 23):
     if nums < smallest:
         smallest = nums
     print("Currently the smallest number so far is, ", smallest)
-print("After analysis, the smallest number in the loop is, ", smallest)
-
-run file upon completion
+print("After analysis, the smallest number in the loop is now finally, ", smallest)
