147a8cf2bb49076cd49df1b2b66dcab85f02f27d
FiyidadeA <fiyidads@gmail.com>
2026-02-07 16:20:11 +0000

    clean up repo

--- Floornumber calc.py
+++
@@ -1,9 +0,0 @@
-def computepay(Hours, Rate):
-    if Hours <= 40 :
-        calc = Hours * Rate
-    elif Hours > 40 :
-        calc = ((Hours - 40) * 15) + 400
-    return calc
-
-dave = computepay(50,10)
-print('The salary for Dave this month is ', dave)
